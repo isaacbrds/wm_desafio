e427fd12cb31988e89c482114a4b6b63222da30d
isaacbrds <isaac.brigido@gmail.com>
2023-03-02 10:32:58 -0300

    test: configuring factory and test for employee

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-  
-  factory :user do
-    username "Joe"
-    email "joe@gmail.com"
-    password "blah"
+  factory :employee do
+    name { Faker::Name.name }
+    position { Faker::Company.profession }
+    admission_date { Faker::Date.between(from: '2014-09-23', to: '2023-02-25')  }
   end
+
 end
---
+++ spec/models/employee_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Employee, type: :model do
+  describe 'associations' do
+    it { should belong_to(:vacation).class_name('MenuCategory') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:position) }
+    it { should validate_presence_of(:admission_date) }
+  end
+end
