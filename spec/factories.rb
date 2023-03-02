154a58caa2def2b2d81523a87b8494fcdc9f5bdd
isaacbrds <isaac.brigido@gmail.com>
2023-03-02 10:50:05 -0300

    test: configuring test for Vaction

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
+  factory :vacation do
+    start_date { Faker::Date.between(from: '2014-09-23', to: '2023-02-25')}
+    finish_date { Faker::Date.between(from: '2014-09-23', to: '2023-02-25') }
+    employee 
+  end
+
   factory :employee do
     name { Faker::Name.name }
     position { Faker::Company.profession }
