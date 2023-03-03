58d5885e614b925753098ac88336123edf65ee08
isaacbrds <isaac.brigido@gmail.com>
2023-03-03 18:34:23 -0300

    test: configuring test validation for models

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,5 +10,4 @@ FactoryBot.define do
     position { Faker::Company.profession }
     admission_date { Faker::Date.between(from: '2014-09-23', to: '2023-02-25')  }
   end
-
 end
--- spec/models/vacation_spec.rb
+++ spec/models/vacation_spec.rb
@@ -2,31 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Vacation, type: :model do
   describe 'associations' do
-    it { should belong_to(:employee) }
+    it { is_expected.to belong_to(:employee) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:start_date) }
-    it { should validate_presence_of(:finish_date) }
+    it { is_expected.to validate_presence_of(:start_date) }
+    it { is_expected.to validate_presence_of(:finish_date) }
   end
 
-  describe 'custom validations' do
-    let(:employee){create(:employee, admission_date: Date.today)}
-    let(:vacation){create(:vacation, start_date: Date.today)}
-    
-    it 'should have one year for vacation rights' do
+  describe 'custom validations with valid params' do
+    let(:employee){create(:employee, admission_date: Date.today.years_ago(1))}
+    let(:vacation){create(:vacation, employee: employee, 
+    start_date: Date.today, finish_date: Date.today + 30.day )}
+
+    it "employee should have one year of work" do
       expect(vacation).to be_valid
     end
 
-    it 'should have a max of 30 days for year' do
-      vacation.start_date =  Date.today 
-      vacation.finish_date = Date.today + 30.day
+    it "start_date should be less than finish date" do  
       expect(vacation).to be_valid
     end
 
-    it 'should have a min of 10 days for vacation period' do
-      vacation.start_date =  Date.today 
-      vacation.finish_date = Date.today + 10.day
+    it "period should be at least ten days" do
+      expect(vacation).to be_valid
+    end
+
+    it "period should be a max of thirty days" do
       expect(vacation).to be_valid
     end
   end
